60ef6447d734c4fbc484f319f450669e7e6c7962
nnguye59 <nnguyen59@uncc.edu>
2019-09-05 18:17:17 -0400

    Added inputs for newsletter

--- resources/views/templates/newsletter.blade.php
+++ resources/views/templates/newsletter.blade.php
@@ -3,11 +3,19 @@
     <div class="newsletter-form mt-3 mt-md-4 mt-lg-5">
         <form action="/subcribe" method="POST" class="form-inline">
             @csrf
-            <div class="input-group w-100">
-                <input type="email" placeholder="Your email address" class="form-control mr-3">
-                <span class="input-group-btn">
+            <div class="form-row w-100">
+                <div class="col-lg-3 col-6">
+                    <input type="text" placeholder="First Name" class="form-control">
+                </div>
+                <div class="col-lg-3 col-6">
+                    <input type="text" placeholder="Last Name" class="form-control">
+                </div>
+                <div class="col-lg-4 mt-3 mt-lg-0">
+                    <input type="email" placeholder="Email Address" class="form-control">
+                </div>
+                <div class="col-lg-2 mt-3 mt-lg-0">
                     <button type="submit">Subcribe</button>
-                </span>
+                </div>
             </div>
         </form>
     </div>
