9d78c73bf04187fe37b9eed1fe9f35b8c79b432f
nnguye59 <nnguyen59@uncc.edu>
2019-09-05 15:55:30 -0400

    Decorated newsletter

--- resources/views/templates/newsletter.blade.php
+++ resources/views/templates/newsletter.blade.php
@@ -1,9 +1,14 @@
 <div class="container-fluid">
-    <div class="newsletter-title">Subcribe for our newsletter. Keep you updated with upcoming events</div>
-    <div class="newsletter-form">
-        <form action="/subcribe" method="POST" class="">
+    <div class="newsletter-title font-family--lora font-size--md">Subcribe for our newsletter. Keep you updated with upcoming events</div>
+    <div class="newsletter-form mt-3 mt-md-4 mt-lg-5">
+        <form action="/subcribe" method="POST" class="form-inline">
             @csrf
-            <input type="email" placeholder="Your email address">
-            <button type="submit">SUBSCRIBE</button>
+            <div class="input-group w-100">
+                <input type="email" placeholder="Your email address" class="form-control mr-3">
+                <span class="input-group-btn">
+                    <button type="submit">Subcribe</button>
+                </span>
+            </div>
+        </form>
     </div>
 </div>
