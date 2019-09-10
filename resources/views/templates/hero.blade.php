5ce5d6d8430dc69e4238dad9723c826aec3f815a
nnguye59 <nnguyen59@uncc.edu>
2019-09-09 21:40:16 -0400

    Added tailwin framework

--- resources/views/templates/hero.blade.php
+++ resources/views/templates/hero.blade.php
@@ -1,4 +1,31 @@
-<div class="container-fluid">
+<div class="container p-4">
+    <div class="flex">
+        <div class="w-1/2">
+            <nav class="flex items-center justify-between flex-wrap">
+                <div class="block lg:hidden">
+                    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
+                </div>
+                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div class="text-sm lg:flex-grow">
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red mr-4">
+                        Docs
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red mr-4">
+                        Examples
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red">
+                        Blog
+                    </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container-fluid">
     <div class="hero-nav row no-gutters">
         <div class="col-lg-8 col-12">
             <nav class="navbar navbar-expand-lg w-100" id="left-nav">
@@ -70,4 +97,4 @@
 <a class="hero-scroll text-color--white" href="#about">
     Scroll
     <span class="scroll-line"></span>
-</a>
+</a> --}}
